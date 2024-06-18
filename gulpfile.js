e556ec1fb169311880eee23ca314d73d3352a0f7
NicolasRojas <nicrojasc6@gmail.com>
2024-06-18 12:18:12 -0500

    [ADD] configurando SASS y gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-import { src, dest} from 'gulp'
+import { src, dest, watch } from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
@@ -7,7 +7,16 @@ const sass = gulpSass(dartSass)
 export function css( done ) {
     //Ubica el archivo
     src('src/scss/app.scss')
-        .pipe( sass() ) //Asi le decimos que lo compile
+        .pipe( sass().on('error', sass.logError) ) //Asi le decimos que lo compile
         .pipe( dest('build/css')) //El destino
     done()
+}
+//No se le pone done por que es un watch y siempre esta a la espera de cambios
+export function dev() {
+    //que este pendiente de esta hoja de estilos y ejecute la func css
+    watch('src/scss/**/*.scss', css)
+    /** los asteriscos indican que busque en todas las carpetas que
+     * estan dentro de scss y que busque todos los archivos con
+     * extension .scss
+     */
 }
