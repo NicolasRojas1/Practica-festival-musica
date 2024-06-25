eff9059933bfadee9b14eca28bdc8e3a15a21e36
NicolasRojas <nicrojasc6@gmail.com>
2024-06-24 21:03:45 -0500

    [ADD] modal en galeria

--- src/js/app.js
+++ src/js/app.js
@@ -15,8 +15,44 @@ function crearGaleria() {
         //La ubicacion de la imagen 
         imagen.src = `src/img/gallery/full/${i}.jpg`;
         imagen.alt = 'Imagen Galería';
-        
+
+        //Event handler, encargado de detectar y responder a una interaccion
+        imagen.onclick = function() {
+            mostrarImagen(i);
+        }
         //Agregando imagenes
         galeria.appendChild(imagen)
     }
+}
+
+function mostrarImagen(i) {
+    //Selecciono la imagen
+    const imagen = document.createElement('IMG');
+    imagen.src = `src/img/gallery/full/${i}.jpg`;
+    imagen.alt = 'Imagen Galería';
+    
+    //Generar Modal
+    const modal = document.createElement('DIV');
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal; 
+    //ya con el modal, agrego la imagen
+    modal.appendChild(imagen);
+
+    //agregar al HTML
+    const body = document.querySelector('body')
+    //creo esta clase para quitar la barra lateral
+    body.classList.add('overflow-hidden');
+    body.appendChild(modal)
+}
+
+function cerrarModal() {
+    const modal = document.querySelector('.modal');
+    modal.classList.add('fade-out')
+
+    setTimeout(() => {
+        modal?.remove(); //si existe el modal quitalo
+        const body = document.querySelector('body')
+        //asi elimino la clase
+        body.classList.remove('overflow-hidden');
+    }, 500) //en medio segundo
 }
